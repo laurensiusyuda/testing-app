2706758ab398a81d88f6db785f4c44ff06217aac
laurensiusyuda <yudalaurensius87@gmail.com>
2025-07-25 20:35:39 +0700

    delete : migration and add mgration

--- database/migrations/2025_04_21_080140_create_penjualans_table.php
+++ database/migrations/2025_04_21_080140_create_penjualans_table.php
@@ -9,17 +9,17 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-public function up()
-{
-    Schema::create('penjualans', function (Blueprint $table) {
-        $table->id();
-        $table->foreignId('barang_id')->constrained()->onDelete('cascade');
-        $table->integer('jumlah');
-        $table->integer('total_harga')->after('jumlah')->nullable();
-        $table->date('tanggal');
-        $table->timestamps();
-    });
-}
+    public function up()
+    {
+        Schema::create('penjualans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('barang_id')->constrained()->onDelete('cascade');
+            $table->integer('jumlah');
+            $table->integer('total_harga')->after('jumlah')->nullable();
+            $table->date('tanggal')->nullable()->after('total_harga');
+            $table->timestamps();
+        });
+    }
 
 
 
